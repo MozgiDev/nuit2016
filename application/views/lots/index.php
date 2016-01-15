60c7155ce26e924c6ebe08b7253d6d2e0d04e182
Kohadon <jeremie.pillot@gmail.com>
2016-01-16 00:12:50 +0100

    correction controller contenu

---
+++ application/views/lots/index.php
@@ -0,0 +1,19 @@
+<nav>
+	<div class="nav-wrapper">
+		<a href="#" class="brand-logo center">Back Office</a>
+	</div>
+</nav>
+
+<nav>
+	<div class="nav-wrapper">
+		<a href="#" class="brand-logo center">Logo</a>
+		<ul id="nav-mobile" class="left hide-on-med-and-down">
+			<li><a href="#">Gestions des contenus</a></li>
+			<li><a href="#">Gestions des joueurs</a></li>
+			<li><a href="#">Gestion des ateliers</a></li>
+			<li><a href="#">Gestion des associations</a></li>
+		</ul>
+	</div>
+</nav>
+
+
