3e02caebb291d889b95350ca5afbe0ef0d622f40
ilias <iliass5165@gmail.com>
2016-01-16 00:05:37 +0100

    modification du controlleur atelier + ajout de son modele

--- application/controllers/Atelier.php
+++ application/controllers/Atelier.php
@@ -3,8 +3,48 @@
 class Atelier extends CI_Controller {
 
 
+	public function __construct()
+	{
+		parent:: __construct();
+		$this->load->model('atelier_model', 'atelierManager');
+	}
 	public function ajouter()
 	{
+		$data['title'] = 'Ajouter un atelier';
+		$this->template->load('layouts/admin', 'atelier/ajouter', $data);
+	}
 
+	public function store()
+	{
+		$data['libelle'] = $this->input->post('libelle');
+		$data['description'] = $this->input->post('description');
+		$data['afficher'] = $this->input->post('afficher');
+
+		if($this->atelierManager->save($data))
+		{
+			redirect('atelier/lister');
+		}
+	}
+	//pour l'admin
+	public function lister()
+	{
+		$data = [];
+		$data['title'] = 'List des ateliers';
+		$data['ateliers'] = $this->ateliersManager->all();
+		$this->template->load('layouts/admin', 'atelier/ajouter', $data);
+	}
+
+	//public
+	public function ateliers()
+	{
+		$data = [];
+		$data['title'] = 'List des ateliers';
+		$data['ateliers'] = $this->ateliersManager->all();
+		$this->template->load('layouts/admin', 'atelier/ajouter', $data);
+	}
+
+	public function supprimer()
+	{
+		$this->atelierManager->
 	}
 }
