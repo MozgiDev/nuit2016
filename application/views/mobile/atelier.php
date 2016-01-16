76c499d35de70a6184f9007d5568de56d9f8b8f7
ilias <iliass5165@gmail.com>
2016-01-16 06:51:45 +0100

    lala

---
+++ application/views/mobile/atelier.php
@@ -0,0 +1,21 @@
+<div class="input-field col s12">
+<table class='responsive-table'>
+	<thead>
+	<th>Atelier</th>	
+	<th>Libelle</th>	
+	<th>Description</th>	
+	<th>action</th>	
+	</thead>
+
+	<tbody>
+		<?php 
+		foreach($ateliers as $unAtelier)
+		{?>
+
+			
+
+		<?php}	
+		?>
+	</tbody>
+</table>
+</div>
--- application/views/mobile/joueur.php
+++
@@ -1,7 +0,0 @@
-<div class="input-field col s12">
-    <select>
-      <?php foreach($joueurs as $unJoueur)
-        echo '<option value=".'$unJoueur->idJoueur.'">'.$unJoueur->pseudo.'</option>';
-      ?>
-    </select>
-  </div>
